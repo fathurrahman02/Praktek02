5d3b725cca2ab0b0ac13248c32c83e48956dbda1
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-03-30 02:53:46 -0700

    Membuat Atribut dan Behavior

--- src/praktek02/PenjualanRumah.java
+++ src/praktek02/PenjualanRumah.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class PenjualanRumah {
+    int harga_rumah;
+    int jumlah_unit;
     
+     void tampildata(){
+    System.out.println("================================");
+    System.out.println("harga_rumah     :"+harga_rumah);
+    System.out.println("jumlah_unit     :"+jumlah_unit);
+    System.out.println("================================");
+ }
 }
