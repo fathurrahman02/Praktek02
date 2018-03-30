40ce629ea67fa9e5e1e2600428f59bad6064103d
fathurrahman02 <37796047+fathurrahman02@users.noreply.github.com>
2018-03-30 03:03:40 -0700

    Mengisi Nilai atribut

--- src/praktek02/PenjualanRumahAksi.java
+++ src/praktek02/PenjualanRumahAksi.java
@@ -3,5 +3,7 @@ package praktek02;
 public class PenjualanRumahAksi {
     public static void main(String[] args) {
         PenjualanRumah r1 = new PenjualanRumah();
+        r1.harga_rumah = 100000000;
+        r1.jumlah_unit = 7;
     }
 }
